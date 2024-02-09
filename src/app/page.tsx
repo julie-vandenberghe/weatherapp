496a1e13fb3b834eae6029d710637809cead1e42
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-02-09 22:29:01 +0100

    ➕ add react-icons dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
     </div>
   );

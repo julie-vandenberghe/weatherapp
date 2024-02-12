3ebf6f91cb08f68d0b14d8bc73b9edd683a5fbc0
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-02-12 11:23:05 +0100

    ✨ loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import axios from "axios";
 import Image from "next/image";
 import { useQuery } from "react-query";
 //https://api.openweathermap.org/data/2.5/forecast?q=&pune&appid=c2b84193fe521c38dada6f68da0ef9de&cnt=56
@@ -62,20 +65,32 @@ interface CityInfo {
   sunset: number;
 }
 
-
 export default function Home() {
-  const { isLoading, error, data } = useQuery('repoData', async() =>
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
-    ).then(res =>
-      res.json()
-    )
-  )
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    }
 
-  if (isLoading) return 'Loading...';
+    /* fetch(`https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+    ).then(res =>  res.json()) */
+    // Because we're using axios, we don't need to use fetch and to convert it into json
+  );
+
+  console.log("data", data?.city.name);
+
+  if (isLoading) return (
+  <div className="flex items-center min-h-screen justify-center">
+    <p className="animate-bounce">Loading...</p>
+    </div>);
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main></main>
     </div>
   );
 }
